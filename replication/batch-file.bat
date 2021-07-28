533599efdd73eeae7558857c4fc2c212ffed7ce0
Alizamin Jafarli <20624518+AlizaminJ@users.noreply.github.com>
2021-07-28 15:42:43 +0200

    Create batch-file.bat

---
+++ replication/batch-file.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+ECHO ============================
+ECHO Replicating data
+cd <SCRIPT-DIRECTORY>
+"<INCLUDE-YOUR-PATH-TO-PYTHON\venv\Scripts\python.exe>" "INCLUDE-YOUR-PATH-TO-PY-FILE.py"
+ECHO Syncing with AWS S3 bucket
+aws s3 sync <PATH-TO-FILES> <s3://YOUR-BUCKET> --delete --profile <YOUR-AWS-CLI-PROFILE>
+EXIT
