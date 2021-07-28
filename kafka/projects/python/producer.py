6ea3a424ac0c15fc470d31862c335d77d118a1a6
AlizaminJ <jafarli_alizamin@yahoo.com>
2021-07-28 16:42:33 +0200

    projects

---
+++ kafka/projects/python/producer.py
@@ -0,0 +1,16 @@
+import time
+from kafka import KafkaProducer
+from faker import Faker
+fake = Faker()
+producer = KafkaProducer(bootstrap_servers=['localhost:9092', 'localhost:9093', 'localhost:9094'])
+# for _ in range(100):
+#     name = fake.name()
+#     producer.send('names', name.encode('utf-8'))
+#     print(name)
+# time.sleep(20)
+
+while True:
+    name = fake.name()
+    producer.send('names', name.encode('utf-8'))
+    print(name)
+    time.sleep(2)
