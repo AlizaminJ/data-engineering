4b45cf4c1cbb228c1237ca6c26b25fce65597bde
Alizamin Jafarli <20624518+AlizaminJ@users.noreply.github.com>
2021-07-28 15:44:08 +0200

    Update batch-file.bat

--- replication/batch-file.bat
+++ replication/batch-file.bat
@@ -4,5 +4,5 @@ ECHO Replicating data
 cd <SCRIPT-DIRECTORY>
 "<INCLUDE-YOUR-PATH-TO-PYTHON\venv\Scripts\python.exe>" "INCLUDE-YOUR-PATH-TO-PY-FILE.py"
 ECHO Syncing with AWS S3 bucket
-aws s3 sync <PATH-TO-FILES> <s3://YOUR-BUCKET> --delete --profile <YOUR-AWS-CLI-PROFILE>
+aws s3 sync <PATH-TO-FOLDER-CONTAINING-REPLICATED-FILES> <s3://YOUR-BUCKET> --delete --profile <YOUR-AWS-CLI-PROFILE>
 EXIT
